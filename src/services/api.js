09c6d0835848e12d5414dd399b77043af6289faf
subhanali <117721076+Subhanali000@users.noreply.github.com>
2025-03-15 06:43:25 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/coupons';
+const API_URL = 'http://localhost:3000';
 
 export const getRandomCoupon = async () => {
     try {
