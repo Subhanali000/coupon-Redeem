d1f7dc10209b95ba119ef3c24dbd9801acd15899
subhanali <117721076+Subhanali000@users.noreply.github.com>
2025-03-15 06:33:49 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/coupons';
+const API_URL = 'https://coupon-backend-6us5.onrender.com/';
 
 export const getRandomCoupon = async () => {
     try {
